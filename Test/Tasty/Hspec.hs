b0573925dbe01607a676ffa29566742af961d7e4
Mitchell Rosen <mitchellwrosen@gmail.com>
2014-09-10 22:36:48 -0700

    update for hspec-1.11

--- Test/Tasty/Hspec.hs
+++ Test/Tasty/Hspec.hs
@@ -1,79 +1,84 @@
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE DeriveDataTypeable #-}
 
 module Test.Tasty.Hspec
-    ( testCase
-
+    ( testSpec
     -- * Re-exports
     , module Test.Hspec
-    , Test.Tasty.TestName
-    , Test.Tasty.TestTree
     ) where
 
-import           Control.Applicative ((<$>))
-import           Data.Typeable       (Typeable)
-import           Test.Hspec          (Spec)
-import           Test.Hspec.Core
-import           Test.Hspec.Runner   (Config(..), defaultConfig)
-import           Test.QuickCheck     (Args)
+import           Control.Applicative    ((<$>))
+import           Control.Monad          (join)
+import           Data.Proxy
+import           Data.Typeable          (Typeable)
+import qualified Test.Hspec             as H
+import qualified Test.Hspec.Core        as H
+import qualified Test.Hspec.Runner      as H
+import qualified Test.QuickCheck        as QC
+import qualified Test.QuickCheck.Random as QC
+import qualified Test.Tasty             as T
+import qualified Test.Tasty.SmallCheck  as TSC
+import qualified Test.Tasty.Options     as T
+import qualified Test.Tasty.Providers   as T
+import qualified Test.Tasty.QuickCheck  as TQC
+import           System.Random
 
-import           Test.Tasty           (TestName, TestTree, testGroup)
-import           Test.Tasty.Providers (IsTest(..), singleTest, testPassed, testFailed)
-import qualified Test.Tasty.Providers as T
+import Test.Hspec -- for re-export
 
--- | Turn an hspec @Spec@ into a tasty @TestTree@.
---
--- > module AnimalsSpec (tests) where
--- >
--- > import Test.Tasty.Hspec
--- >
--- > tests :: TestTree
--- > tests = testGroup "animals"
--- >     [ testCase "mammals" mammalsSpec
--- >     , testCase "birds"   birdsSpec
--- >     ]
--- >
--- > mammalsSpec :: Spec
--- > mammalsSpec = do
--- >     describe "cow" $ do
--- >         it "moos" $
--- >             speak cow `shouldBe` "moo"
--- >
--- >         it "eats grass" $
--- >             hungryFor cow `shouldBe` "grass"
--- >
--- > birdsSpec :: Spec
--- > birdsSpec = do
--- >     describe "ostrich" $ do
--- >         it "sticks its head in sand" $
--- >             fmap (`shouldBe` InSand) getHeadState
-testCase :: TestName -> Spec -> TestTree
-testCase name = testGroup name . map specTreeToTestTree . runSpecM
+-- | Create a tasty 'T.TestTree' from an HSpec 'H.Spec'.
+testSpec :: T.TestName -> H.Spec -> IO T.TestTree
+testSpec name spec = H.runSpecM spec >>= fmap (T.testGroup name . join) . mapM specTreeToTestTrees
 
--- Convert a hspec SpecTree into a tasty TestTree.
-specTreeToTestTree :: SpecTree -> TestTree
-specTreeToTestTree (SpecGroup name specs) = testGroup name (map specTreeToTestTree specs)
-specTreeToTestTree (SpecItem (Item _ name example)) = singleTest name (MyExample example)
+specTreeToTestTrees :: H.SpecTree -> IO [T.TestTree]
+specTreeToTestTrees (H.SpecGroup name specs) = return . T.testGroup name . join <$> mapM specTreeToTestTrees specs
+specTreeToTestTrees (H.BuildSpecs action)    = action >>= fmap join . mapM specTreeToTestTrees
+specTreeToTestTrees (H.SpecItem name item)   = return [T.singleTest name (Item item)]
 
-newtype MyExample = MyExample (Params -> (IO () -> IO ()) -> IO Result) deriving Typeable
+hspecResultToTastyResult :: H.Result -> T.Result
+hspecResultToTastyResult H.Success        = T.testPassed ""
+hspecResultToTastyResult (H.Pending mstr) = T.testFailed ("test pending" ++ maybe "" (": " ++) mstr)
+hspecResultToTastyResult (H.Fail str)     = T.testFailed str
 
-instance IsTest MyExample where
-    run _ (MyExample f) _ = hspecResultToTastyResult <$> f params id
+newtype Item = Item H.Item
+    deriving Typeable
+
+instance T.IsTest Item where
+    run opts (Item (H.Item _ example)) progress =
+        hspecResultToTastyResult <$> example params id hprogress
       where
-        hspecResultToTastyResult :: Result -> T.Result
-        hspecResultToTastyResult Success     = testPassed ""
-        hspecResultToTastyResult (Pending _) = testFailed "(test pending)"
-        hspecResultToTastyResult (Fail str)  = testFailed str
+        params :: H.Params
+        params = H.Params
+            { H.paramsQuickCheckArgs = qc_args
+            , H.paramsSmallCheckDepth = sc_depth
+            }
+          where
+            qc_args :: QC.Args
+            qc_args =
+                let TQC.QuickCheckTests    num_tests = T.lookupOption opts
+                    TQC.QuickCheckReplay   replay    = T.lookupOption opts
+                    TQC.QuickCheckMaxSize  max_size  = T.lookupOption opts
+                    TQC.QuickCheckMaxRatio max_ratio = T.lookupOption opts
+                in QC.stdArgs
+                    { QC.chatty          = False
+                    , QC.maxDiscardRatio = max_ratio
+                    , QC.maxSize         = max_size
+                    , QC.maxSuccess      = num_tests
+                    , QC.replay          = replay
+                    }
 
-        params :: Params
-        params = Params quickCheckArgs smallCheckDepth reportProgress
+            sc_depth :: Int
+            sc_depth = let TSC.SmallCheckDepth depth = T.lookupOption opts in depth
 
-        quickCheckArgs :: Args
-        quickCheckArgs = configQuickCheckArgs defaultConfig
+        hprogress :: H.Progress -> IO ()
+        hprogress (x,y) = progress $ T.Progress
+            { T.progressText    = ""
+            , T.progressPercent = fromIntegral x / fromIntegral y
+            }
 
-        smallCheckDepth :: Int
-        smallCheckDepth = configSmallCheckDepth defaultConfig
-
-        reportProgress :: Progress -> IO ()
-        reportProgress _ = return ()
-
-    testOptions = return []
+    testOptions = return
+        [ T.Option (Proxy :: Proxy TQC.QuickCheckTests)
+        , T.Option (Proxy :: Proxy TQC.QuickCheckReplay)
+        , T.Option (Proxy :: Proxy TQC.QuickCheckMaxSize)
+        , T.Option (Proxy :: Proxy TQC.QuickCheckMaxRatio)
+        , T.Option (Proxy :: Proxy TSC.SmallCheckDepth)
+        ]
