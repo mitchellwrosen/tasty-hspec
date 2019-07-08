7fa23cee662218bfa7fb5f05446bed8e37d7a1e6
Mitchell Rosen <mitchellwrosen@gmail.com>
2019-07-07 21:50:24 -0400

    fix travis

--- test/Main.hs
+++ test/Main.hs
@@ -6,15 +6,14 @@ main :: IO ()
 main = do
   tree <- testSpec "spec" spec
   defaultMain tree
-  -- hspec spec
 
 spec :: Spec
 spec = do
   it "success" ('a' == 'a')
-  it "pending" pending
-  it "pendingWith" (pendingWith "foo")
-  it "failure (no reason)" ('a' == 'b')
-  it "failure (reason)" (expectationFailure "foo")
-  it "failure (expected but got)" ('a' `shouldBe` 'b')
-  it "failure (uncaught exception)" (fail "nope" :: IO ())
+  -- it "pending" pending
+  -- it "pendingWith" (pendingWith "foo")
+  -- it "failure (no reason)" ('a' == 'b')
+  -- it "failure (reason)" (expectationFailure "foo")
+  -- it "failure (expected but got)" ('a' `shouldBe` 'b')
+  -- it "failure (uncaught exception)" (fail "nope" :: IO ())
   -- fit "focused" ('a' == 'a')
